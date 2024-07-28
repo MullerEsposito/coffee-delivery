b9a3f442b477efcc05915af9d487e1da6936133d
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-07-28 18:48:24 -0300

    feat(Checkout|CoffeeCard): it implements functionality to the component so fetch the data of the coffees in cart and mount in the component;

--- src/pages/Checkout/CoffeeCard/index.tsx
+++ src/pages/Checkout/CoffeeCard/index.tsx
@@ -1,28 +1,42 @@
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useContext, useEffect, useState } from "react";
 import { InputNumber } from "../../../components/InputNumber";
 import { ActionsContainer, CoffeeCardContainer, RemoveButton } from "./style";
 import { Trash } from "@phosphor-icons/react";
 import { useTheme } from "styled-components";
+import { CoffeeInCart } from "../../../@types/coffee";
+import { CartContext } from "../../../contexts/CartContext";
 
 interface CoffeeCardProps {
   containerProps?: HTMLAttributes<HTMLDivElement>;
+  coffeeInCart: CoffeeInCart;
 }
 
-export function CoffeeCard({ containerProps }: CoffeeCardProps) {
+export function CoffeeCard({ containerProps, coffeeInCart }: CoffeeCardProps) {
+  const [inputValue, setInputValue] = useState(coffeeInCart.quantity);
+  const { coffees, removeFromCart, addToCart } = useContext(CartContext);
   const theme = useTheme();
 
-  return (
+  const coffee = coffees.find(coffee => coffee.id === coffeeInCart.id);
+  
+  const coffeePriceReduced = (coffee!.price * inputValue).toFixed(2);
+  
+  useEffect(() => {
+    addToCart({ id: coffee!.id, quantity: inputValue, price: coffee!.price });
+  }, [inputValue])
+
+
+  return coffee && (
     <CoffeeCardContainer {...containerProps}>
-      <img src="/assets/coffees/americano.png" style={{ gridArea: "img" }} />
-      <p style={{ gridArea: "title"}}>Expresso Tradicional</p>
+      <img src={coffee.image} style={{ gridArea: "img" }} />
+      <p style={{ gridArea: "title"}}>{coffee.title}</p>
       <ActionsContainer style={{ gridArea: "controls" }}>
-        <InputNumber min={0}/>
-        <RemoveButton>
+        <InputNumber min={0} defaultValue={coffeeInCart.quantity} getValue={setInputValue}/>
+        <RemoveButton onClick={() => removeFromCart(coffee.id)}>
           <Trash size={16} fill={theme.colors.purple} />
           REMOVER
         </RemoveButton>
       </ActionsContainer>
-      <span style={{ gridArea: "price"}}>R$ 9,90</span>
+      <span style={{ gridArea: "price"}}>R$ {coffeePriceReduced}</span>
     </CoffeeCardContainer>
   )
 }
