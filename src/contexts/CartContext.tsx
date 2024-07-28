7eeef1039e7c3ad316ff5ab64de6906d0a8599f3
Müller Esposito Nunes <mulleresposito@hotmail.com>
2024-07-28 18:59:11 -0300

    feat(CartContext): it move the data of the coffees to be loaded by the CartProvider. It implements a functions to remove a coffee from the Cart. It adds the functionality to insert and retrieve the data from the local storage;

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,14 +1,12 @@
-import { createContext, ReactNode, useState } from "react";
-
-export type CoffeeInCart = {
-  id: string;
-  price: number;
-  quantity: number;
-}
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { coffees as data } from "../data/index.json";
+import { Coffee, CoffeeInCart } from "../@types/coffee";
 
 interface CartContextType {
+  coffees: Coffee[];
   coffeesInCart: CoffeeInCart[];
   addToCart: (coffee: CoffeeInCart) => void;
+  removeFromCart: (id: string) => void;
 }
 
 export const CartContext = createContext({} as CartContextType);
@@ -18,27 +16,49 @@ interface CartProviderProps {
 }
 
 export function CartProvider({ children }: CartProviderProps) {
+  const [coffees, setCoffees] = useState(data as Coffee[]);
   const [coffeesInCart, setCoffeesInCart] = useState<CoffeeInCart[]>([]);
+
+  useEffect(() => {
+    const coffeesInCartLocalStorage = localStorage.getItem("@muller-coffee-delivery:coffees-in-cart-1.0.0");
+  
+    if (coffeesInCartLocalStorage)
+      setCoffeesInCart(JSON.parse(coffeesInCartLocalStorage));
+
+  }, [])
+
+  useEffect(() => {
+    const coffeesInCartJSON = JSON.stringify(coffeesInCart);
+    localStorage.setItem("@muller-coffee-delivery:coffees-in-cart-1.0.0", coffeesInCartJSON);
+  }, [coffeesInCart])
 
   const addToCart = (coffee: CoffeeInCart) => {
     setCoffeesInCart(old => {
       const coffeeFound = old.find(({id}) => id === coffee.id);
       
-      if (coffeeFound) {
-        console.log(`Quantidade do Input: ${coffee.quantity}`);
-        
-        coffeeFound.quantity += coffee.quantity;
-        console.log(`Quantidade: ${coffeeFound.quantity}`);
-        
-        return old.map((coffeeOld) => coffeeOld.id === coffee.id ? coffeeFound : coffeeOld);
+      if (coffeeFound) { 
+        if (coffee.quantity > 0) {
+          return old.map(coffeeOld => coffeeOld.id === coffee.id ? coffee : coffeeOld);
+        }
+       return old.filter(coffeeOld => coffeeOld.id !== coffee.id);
       } else {
         return [...old, coffee];
       }
     });
   }
 
+  const removeFromCart = (id: string) => {
+    setCoffeesInCart(olds => {
+      const coffeeFound = olds.find(old => old.id === id);
+      
+      if (coffeeFound)
+        return olds.filter(old => old.id !== id);
+      return olds;
+    });
+  }
+
   return (
-    <CartContext.Provider value={{ coffeesInCart, addToCart }}>
+    <CartContext.Provider value={{ coffees, coffeesInCart, addToCart, removeFromCart }}>
       { children }
     </CartContext.Provider>
   )
